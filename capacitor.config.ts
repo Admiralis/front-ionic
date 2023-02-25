335f3b75a22d2295e9d61f67973a6b03b50d484f
0h My Goat <daftouney@gmail.com>
2023-02-25 13:04:22 +0100

    chore: config projet + android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'admiralis',
   appName: 'front-ionic',
   webDir: 'build',
   bundledWebRuntime: false
