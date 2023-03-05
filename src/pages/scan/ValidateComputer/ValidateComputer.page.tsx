070fde65b1eb4ab9f9dbd2c0433e5658ff470942
0h My Goat <daftouney@gmail.com>
2023-03-05 15:53:59 +0100

    refactor: imports :recycle:

--- src/pages/scan/ValidateComputer/ValidateComputer.page.tsx
+++ src/pages/scan/ValidateComputer/ValidateComputer.page.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {IonContent, IonPage} from "@ionic/react";
-import {CardComponent} from "../../../commons/components";
+import {CardComponent} from "commons/components";
 import {useLocation} from "react-router";
-import {NewComputer} from "../../../commons/models";
+import {NewComputer} from "commons/models";
 
 const ValidateComputerPage = () => {
 
