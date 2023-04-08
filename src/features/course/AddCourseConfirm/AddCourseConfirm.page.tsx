26b35f779fdddfebd60bf20876befa78548864e3
0h My Goat <daftouney@gmail.com>
2023-04-08 13:58:01 +0200

    fix: affichage modale :bug: :lipstick:

--- src/features/course/AddCourseConfirm/AddCourseConfirm.page.tsx
+++ src/features/course/AddCourseConfirm/AddCourseConfirm.page.tsx
@@ -14,7 +14,6 @@ const AddCourseConfirmPage = () => {
     return (
         <IonPage>
             <IonContent>
-
                 <form className="flex-container">
                     <CardComponent
                         title="Valider"
