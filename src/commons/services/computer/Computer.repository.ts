38af4f2a3a26389af6e4ccf5a8a98c8654009233
0h My Goat <daftouney@gmail.com>
2023-04-21 16:45:35 +0200

    docs: ajout documentation :memo:

--- src/commons/services/computer/Computer.repository.ts
+++ src/commons/services/computer/Computer.repository.ts
@@ -3,21 +3,36 @@ import {Computer} from "../../models";
 class ComputerRepository {
     private url = 'http://localhost/api/computers';
 
+    /**
+     * Récupère la liste des ordinateurs
+     */
     async findAll(): Promise<Computer[]> {
         const response = await fetch(this.url);
         return await response.json();
     }
 
+    /**
+     * Récupère un ordinateur par son id
+     * @param id
+     */
     async findById(id: string): Promise<Computer> {
         const response = await fetch(`${this.url}/${id}`);
         return await response.json();
     }
 
+    /**
+     * Recherche un ordinateur par son numéro de série
+     * @param serial
+     */
     async findBySerial(serial: string): Promise<Computer> {
         const response = await fetch(`${this.url}/search?serialNumber=${serial}`);
         return await response.json();
     }
 
+    /**
+     * Enregistre un ordinateur
+     * @param computer
+     */
     async save(computer: Computer): Promise<Computer> {
         const response = await fetch(this.url, {
             method: 'POST',
@@ -29,6 +44,10 @@ class ComputerRepository {
         return await response.json();
     }
 
+    /**
+     * Ecrase les informations d'un ordinateur
+     * @param computer
+     */
     async replace(computer: Computer): Promise<Computer> {
         const response = await fetch(`${this.url}/${computer.id}`, {
             method: 'PUT',
@@ -40,6 +59,10 @@ class ComputerRepository {
         return await response.json();
     }
 
+    /**
+     * Met à jour les informations d'un ordinateur
+     * @param computer
+     */
     async update(computer: Computer): Promise<Computer> {
         const response = await fetch(`${this.url}/${computer.id}`, {
             method: 'PATCH',
@@ -51,6 +74,10 @@ class ComputerRepository {
         return await response.json();
     }
 
+    /**
+     * Supprime un ordinateur par son id
+     * @param id
+     */
     async deleteById(id: string): Promise<void> {
         await fetch(`${this.url}/${id}`, {
             method: 'DELETE',
