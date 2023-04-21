fd07652db7650c806b287daa6115bfd58fc5de42
0h My Goat <daftouney@gmail.com>
2023-04-21 16:42:31 +0200

    docs: ajout documentation :memo:

--- src/commons/services/course/Course.repository.ts
+++ src/commons/services/course/Course.repository.ts
@@ -3,16 +3,27 @@ import {Course} from "../../models";
 class CourseRepository {
     private url = 'http://localhost/api/courses';
 
+    /**
+     * Récupère la liste des cours
+     */
     async findAll(): Promise<Course[]> {
         const response = await fetch(this.url);
         return await response.json();
     }
 
+    /**
+     * Récupère un cours par son id
+     * @param id
+     */
     async findById(id: string): Promise<Course> {
         const response = await fetch(`${this.url}/${id}`);
         return await response.json();
     }
 
+    /**
+     * Enregistre un cours
+     * @param course
+     */
     async save(course: Course): Promise<Course> {
         const response = await fetch(this.url, {
             method: 'POST',
@@ -24,6 +35,11 @@ class CourseRepository {
         return await response.json();
     }
 
+    /**
+     * Recherche un cours par son label et sa date de début
+     * @param label
+     * @param startDate
+     */
     async findByLabelAndStartDate(label: string, startDate: Date): Promise<Course> {
         const response = await fetch(`${this.url}/search?label=${label}&startDate=${startDate}`);
         return await response.json();
