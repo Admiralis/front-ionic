47d2f05d7eb2b08427f94d7b3ccae166c5cc3613
0h My Goat <daftouney@gmail.com>
2023-05-03 19:11:59 +0200

    test: ComputerForm 100% coverage :white_check_mark:

---
+++ src/commons/components/Forms/ComputerForm/ComputerForm.test.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {act, render, screen} from '@testing-library/react';
+import {ionFireEvent as fireEvent, mockIonicReact, waitForIonicReact} from '@ionic/react-test-utils';
+import {setupIonicReact} from '@ionic/react';
+
+import {ComputerFormComponent} from "./ComputerForm.component";
+import {Computer} from "../../../models";
+
+describe('ComputerForm', () => {
+
+    let props: any;
+
+    beforeEach(() => {
+        setupIonicReact();
+        mockIonicReact();
+        const setComputerSerialMock = jest.fn();
+        props = {
+            newComputerInfo: {
+                serialNumber: 'AAAAAAA',
+                category: 'categoryInput',
+                ram: 'ramInput',
+                processor: 'processorInput',
+                condition: 'conditionInput',
+            } as Computer,
+            setNewComputerInfo: setComputerSerialMock
+        }
+    })
+
+    it('should render successfully', () => {
+        const {baseElement} = render(<ComputerFormComponent {...props} />);
+        expect(baseElement).toBeTruthy();
+    });
+
+    it('should change the computer category if the input is changed', async () => {
+        render(<ComputerFormComponent {...props} />);
+        fireEvent.ionChange(screen.getByTestId('input-Catégorie'), 'BBBBBBB');
+        await act(async () => {
+            expect(props.setNewComputerInfo).toHaveBeenCalledWith({
+                ...props.newComputerInfo,
+                category: 'BBBBBBB'
+            });
+        });
+    });
+
+    it('should change the computer ram if the input is changed', async () => {
+        render(<ComputerFormComponent {...props} />);
+        fireEvent.ionChange(screen.getByTestId('input-RAM'), 'BBBBBBB');
+        await act(async () => {
+            expect(props.setNewComputerInfo).toHaveBeenCalledWith({
+                ...props.newComputerInfo,
+                ram: 'BBBBBBB'
+            });
+        });
+    });
+
+    it('should change the computer processor if the input is changed', async () => {
+        render(<ComputerFormComponent {...props} />);
+        fireEvent.ionChange(screen.getByTestId('input-Processeur'), 'BBBBBBB');
+        await act(async () => {
+            expect(props.setNewComputerInfo).toHaveBeenCalledWith({
+                ...props.newComputerInfo,
+                processor: 'BBBBBBB'
+            });
+        });
+    });
+
+    it('should change the computer condition if the input is changed', async () => {
+        render(<ComputerFormComponent {...props} />);
+        fireEvent.ionChange(screen.getByTestId('input-Etat'), 'BBBBBBB');
+        await act(async () => {
+            expect(props.setNewComputerInfo).toHaveBeenCalledWith({
+                ...props.newComputerInfo,
+                condition: 'BBBBBBB'
+            });
+        });
+    });
+
+    it('should have empty inputs if the computer is undefined', async () => {
+        props.newComputerInfo = undefined;
+        render(<ComputerFormComponent {...props} newComputerInfo={ {} as Computer} />);
+        await waitForIonicReact();
+        expect(screen.getByTestId('input-Catégorie')).toHaveValue('');
+        expect(screen.getByTestId('input-RAM')).toHaveValue('');
+        expect(screen.getByTestId('input-Processeur')).toHaveValue('');
+        expect(screen.getByTestId('input-Etat')).toHaveValue('');
+    });
+
+});
--- src/commons/components/Forms/Inputs/AsciiInput/AsciiInput.test.tsx
+++ src/commons/components/Forms/Inputs/AsciiInput/AsciiInput.test.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
 import {act, render, screen} from '@testing-library/react';
 import {ionFireEvent as fireEvent, mockIonicReact} from '@ionic/react-test-utils';
-import {setupIonicReact} from '@ionic/react';
+import {IonInput, setupIonicReact, useIonViewDidEnter} from '@ionic/react';
 
 import {AsciiInputComponent} from "./AsciiInput.component";
+import style from "./AsciiInput.module.css";
+
+// export function AsciiInputComponent(props: AsciiInputComponentProps) {
+//
+//
+//     // Hook de Ionic qui s'exécute à l'affichage du composant.
+//     // Focus l'input si props.autoFocus est passé
+//     useIonViewDidEnter(() => {
+//         if (props.autoFocus) {
+//             const input: HTMLIonInputElement | null = document.querySelector(` #${props.label}`)
+//             if (input?.id === props.label ) input?.setFocus()
+//         }
+//     }, [])
+//
+//     return (
+//         <div className={style.asciiItem} data-testid='asciiInputComponent'>
+//             <span className={props.smallText ? style.asciiSmallLabel : style.asciiLabel}>
+//                 <span>&#62;</span>{props.label}{props.disabled ? <span className={props.smallText? style.littlePrompt : style.prompt }>$ </span> : <span className={props.smallText? style.littlePrompt : style.prompt }>_{props.required && <span>*</span>}</span>}</span>
+//             <IonInput
+//                 id={props.label}
+//                 value={props.value}
+//                 placeholder={props.placeholder}
+//                 onIonChange={props.onIonChange}
+//                 className={props.smallText ? style.asciiSmallInput : style.asciiInput}
+//                 disabled={props.onIonChange === undefined}
+//                 onBlur={props.onBlur}
+//                 data-testid={'input-' + props.label}
+//             />
+//         </div>
+//     );
+// }
 
 describe('AsciiInput', () => {
     let props: any;
+    const onIonChangeMock = jest.fn();
+    const onIonBlurMock = jest.fn();
 
     beforeEach(() => {
         setupIonicReact();
         mockIonicReact();
-        const onIonChangeMock = jest.fn();
-        const onIonBlurMock = jest.fn();
         props = {
             label: 'label',
             value: 'value',
             onIonChange: onIonChangeMock,
-            onIonBlur: onIonBlurMock,
+            onBlur: onIonBlurMock,
         }
     });
 
@@ -61,4 +92,72 @@ describe('AsciiInput', () => {
         expect(label).toBeInTheDocument();
     });
 
+    it('should display the value', () => {
+        render(<AsciiInputComponent {...props} />);
+        const input = screen.queryByTestId('input-' + props.label);
+        expect(input).toHaveValue(props.value);
+    });
+
+    it('should have a placeholder if the placeholder props is defined', () => {
+        render(<AsciiInputComponent {...props} placeholder={'placeholder'}/>);
+        const input = screen.queryByTestId('input-' + props.label);
+        expect(input).toHaveAttribute('placeholder', 'placeholder');
+    });
+
+    it('should have a smaller font if the smallText props is true', () => {
+        render(<AsciiInputComponent {...props} smallText={true}/>);
+        const input = screen.queryByTestId('input-' + props.label);
+        expect(input).toHaveClass('asciiSmallInput');
+    });
+
+    it('should be disabled if the disabled props is true', () => {
+        render(<AsciiInputComponent {...props} disabled={true}/>);
+        const input = screen.queryByTestId('input-' + props.label);
+        expect(input).toBeDisabled();
+    });
+
+    it('should be disabled if onIonChange prop is not set', () => {
+        render(<AsciiInputComponent {...props} onIonChange={undefined}/>);
+        const input = screen.queryByTestId('input-' + props.label);
+        expect(input).toBeDisabled();
+    });
+
+    it('should have a $ prompt if the disabled props is true', () => {
+        render(<AsciiInputComponent {...props} disabled={true}/>);
+        const prompt = screen.queryByText('$');
+        expect(prompt).toBeInTheDocument();
+    });
+
+    it('should have a * prompt if the required props is true', () => {
+        render(<AsciiInputComponent {...props} required={true}/>);
+        const prompt = screen.queryByText('*');
+        expect(prompt).toBeInTheDocument();
+    });
+
+    it('should have a smaller prompt if the smallText props is true', () => {
+        render(<AsciiInputComponent {...props} smallText />);
+        const prompt = screen.getByText('_');
+        expect(prompt).toHaveClass('littlePrompt');
+    });
+
+    it('should have a smaller label if the smallText props is true', () => {
+        render(<AsciiInputComponent {...props} smallText />);
+        const label = screen.getByText(props.label);
+        expect(label).toHaveClass('asciiSmallLabel');
+    });
+
+    it('should have a smaller disabled prompt if the smallText props is true', () => {
+        render(<AsciiInputComponent {...props} smallText disabled />);
+        const prompt = screen.getByText('$');
+        expect(prompt).toHaveClass('littlePrompt');
+    });
+
+    it('should have an id equals to the label', () => {
+        render(<AsciiInputComponent {...props} />);
+        const input = screen.queryByTestId('input-' + props.label);
+        expect(input).toHaveAttribute('id', props.label);
+    });
+
+
 });
+
