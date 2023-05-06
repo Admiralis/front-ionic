10d8af8601615e654e881831557968d1c5dcb33a
0h My Goat <daftouney@gmail.com>
2023-05-06 12:15:04 +0200

    test: autoRescan et Computer :white_check_mark:

--- src/commons/hooks/scan/useAutoRescan.test.ts
+++ src/commons/hooks/scan/useAutoRescan.test.ts
@@ -1,6 +1,7 @@
 import {act, renderHook} from '@testing-library/react'
 
 import useAutoRescan from "./useAutoRescan";
+import {waitForIonicReact} from "@ionic/react-test-utils";
 
 
 // /**
---
+++ src/commons/services/computer/Computer.test.tsx
@@ -0,0 +1,72 @@
+import {act} from '@testing-library/react'
+
+import {mockIonicReact} from '@ionic/react-test-utils'
+
+import ComputerService from "./Computer.service";
+import {Computer, NewComputer} from "../../models";
+import ComputerRepository from "./Computer.repository";
+describe('ComputerService', () => {
+
+    const findAllMock = jest.fn(() => Promise.resolve([]));
+    const findByIdMock = jest.fn((id: string) => Promise.resolve({id: id} as unknown as Computer));
+    const findBySerialMock = jest.fn((serial) => Promise.resolve({serialNumber: serial} as unknown as Computer));
+    const saveMock = jest.fn((computer) => Promise.resolve(computer as unknown as Computer));
+
+    jest.doMock('./Computer.repository', async () => {
+        return {
+            findAll: await findAllMock(),
+            findById: await findByIdMock('ABCDEFG'),
+            findBySerial: await findBySerialMock('ABCDEFG'),
+            save: await saveMock({serialNumber: 'ABCDEFG'} as unknown as NewComputer),
+        }
+    });
+
+
+
+    beforeEach(() => {
+        mockIonicReact();
+
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should find all computers', async () => {
+        const fincComputersSpy = jest.spyOn(ComputerRepository, 'findAll');
+        await ComputerService.findComputers();
+        expect(fincComputersSpy).toHaveBeenCalled();
+    });
+
+    it('should find computer by id', async () => {
+        const findComputerByIdSpy = jest.spyOn(ComputerRepository, 'findById');
+        await ComputerService.findComputerById('ABCDEFG');
+        expect(findComputerByIdSpy).toHaveBeenCalled();
+    });
+
+    it('should find computer by serial', async () => {
+        const findComputerBySerialSpy = jest.spyOn(ComputerRepository, 'findBySerial');
+        await ComputerService.findComputerBySerial('ABCDEFG');
+        expect(findComputerBySerialSpy).toHaveBeenCalled();
+    });
+
+    it('should save computer', async () => {
+        const saveComputerSpy = jest.spyOn(ComputerRepository, 'save');
+        await ComputerService.saveComputer({serialNumber: 'ABCDEFG'} as unknown as NewComputer);
+        expect(saveComputerSpy).toHaveBeenCalled();
+    });
+
+    it('should find or create computer by serial', async () => {
+        const findComputerBySerialSpy = jest.spyOn(ComputerRepository, 'findBySerial');
+        const saveComputerSpy = jest.spyOn(ComputerRepository, 'save');
+        await ComputerService.findOrCreateComputerBySerial({serialNumber: 'ABCDEFG'} as unknown as NewComputer);
+        expect(findComputerBySerialSpy).toHaveBeenCalled();
+        expect(saveComputerSpy).toHaveBeenCalled();
+    });
+
+    it('should check if computer exists by serial', async () => {
+        const findComputerBySerialSpy = jest.spyOn(ComputerRepository, 'findBySerial');
+        await ComputerService.computerExistsBySerial('ABCDEFG');
+        expect(findComputerBySerialSpy).toHaveBeenCalled();
+    });
+});
