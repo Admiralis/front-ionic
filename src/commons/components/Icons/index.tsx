280489aff1054138a727621da5998489f2ae7590
0h My Goat <daftouney@gmail.com>
2023-05-19 09:27:48 +0200

    refactor: extracted barcode icon :recycle:

--- src/commons/components/Icons/index.tsx
+++ src/commons/components/Icons/index.tsx
@@ -1,5 +1,6 @@
 import AlertIconComponent from "./AlertIcon/AlertIcon.component";
 import ComputerIconComponent from "./ComputerIcon/ComputerIcon.component";
 import WheelIconComponent from "./WheelIcon/WheelIcon.component";
+import CodebarIconComponent from "./CodeBarIcon/CodeBarIcon.component";
 
-export {AlertIconComponent, ComputerIconComponent, WheelIconComponent};
+export {AlertIconComponent, ComputerIconComponent, WheelIconComponent, CodebarIconComponent};
