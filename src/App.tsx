4c9525f6ec8b0a3224b620fe00f7093e57127763
0h My Goat <daftouney@gmail.com>
2023-05-19 09:47:34 +0200

    refactor: extract wrapper + apply on routes :recycle:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import {Redirect, Route} from 'react-router-dom';
-import {
-    IonApp,
-    IonRouterOutlet,
-    IonTabBar,
-    IonTabButton,
-    IonTabs,
-    setupIonicReact
-} from '@ionic/react';
+import {IonApp, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact} from '@ionic/react';
 import {IonReactRouter} from '@ionic/react-router';
 
 /* Core CSS required for Ionic components to work properly */
@@ -29,44 +22,19 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 import React from "react";
 
-/* Pages */
-import AddComputerPage from "./features/computer/AddComputer/AddComputer.page";
-import EditComputerPage from "./features/computer/EditComputer/EditComputer.page";
-import FindComputerPage from "./features/computer/FindComputer/FindComputer.page";
-import AddCoursePage from "./features/course/AddCourse/AddCourse.page";
-import CreateCollectiveLoanPage from "./features/loan/CreateCollectiveLoan/CreateCollectiveLoan.page";
-import EndLoanPage from "./features/loan/EndLoan/EndLoan.page";
-import CreateIndividualLoanPage from "./features/loan/CreateIndividualLoan/CreateIndividualLoan.page";
-import EditIndividualLoanPage from "./features/loan/EditIndividualLoan/EditIndividualLoan.page";
-import SettingsPage from "./features/settings/Settings.page";
-
 /* Constants */
 import PATHS from "commons/constants/PATHS";
 import NotFoundPage from "./features/404/NotFound.page";
-import useCheckApiConnection from "./commons/hooks/connection/useCheckApiConnection";
-import scanMenuPage from "./features/computer/ScanMenu/ScanMenu.page";
+
+/* Icons */
 import {CodebarIconComponent, ComputerIconComponent, WheelIconComponent} from "./commons/components/Icons";
+
+/* Wrapper and pages */
+import WrappedPages from "./features/pages.wrapper";
+import SettingsPage from "./features/settings/Settings.page";
 
 setupIonicReact();
 
-
-
-
-
-
-const withConnectionStatus = (Component: React.FC) => (props: any) => {
-
-    const {isLoanApiConnected} = useCheckApiConnection();
-
-    console.log('Connected ?', isLoanApiConnected);
-
-    // if (!isLoanApiConnected) return <SettingsPage/>;
-    if (!isLoanApiConnected) return <Redirect to={PATHS.SETTINGS.root}/>;
-    return <Component {...props}/>;
-
-}
-
-const WrappedRoutes = withConnectionStatus(scanMenuPage);
 
 const App: React.FC = () => {
 
@@ -76,25 +44,25 @@ const App: React.FC = () => {
                 <IonTabs>
                     <IonRouterOutlet>
 
-                        <Route exact path={PATHS.SCAN.root} component={WrappedRoutes}/>
+                        <Route exact path={PATHS.SCAN.root} component={WrappedPages.scanMenu}/>
 
-                        <Route exact path={PATHS.SCAN.newComputer} component={AddComputerPage}/>
-                        <Route exact path={PATHS.COMPUTERS.new} component={EditComputerPage}/>
+                        <Route exact path={PATHS.SCAN.newComputer} component={WrappedPages.addComputer}/>
+                        <Route exact path={PATHS.COMPUTERS.new} component={WrappedPages.editComputer}/>
 
-                        <Route exact path={PATHS.SCAN.editComputer} component={FindComputerPage}/>
-                        <Route exact path={PATHS.COMPUTERS.detail + ":id"} component={EditComputerPage}/>
+                        <Route exact path={PATHS.SCAN.editComputer} component={WrappedPages.findComputer}/>
+                        <Route exact path={PATHS.COMPUTERS.detail + ":id"} component={WrappedPages.editComputer}/>
 
-                        <Route exact path={PATHS.SCAN.newCourse} component={AddCoursePage}/>
-                        <Route exact path={PATHS.LOANS.newCollective} component={CreateCollectiveLoanPage}/>
+                        <Route exact path={PATHS.SCAN.newCourse} component={WrappedPages.addComputer}/>
+                        <Route exact path={PATHS.LOANS.newCollective} component={WrappedPages.createCollectiveLoan}/>
 
-                        <Route exact path={PATHS.SCAN.endLoan} component={FindComputerPage}/>
-                        <Route exact path={PATHS.LOANS.end + ":id"} component={EndLoanPage}/>
+                        <Route exact path={PATHS.SCAN.endLoan} component={WrappedPages.findComputer}/>
+                        <Route exact path={PATHS.LOANS.end + ":id"} component={WrappedPages.endLoan}/>
 
 
                         <Route exact path={PATHS.SCAN.newIndividualLoan}
-                               component={CreateIndividualLoanPage}/>
+                               component={WrappedPages.createIndividualLoan}/>
                         <Route exact path={PATHS.LOANS.confirmIndividual}
-                               component={EditIndividualLoanPage}/>
+                               component={WrappedPages.editIndividualLoan}/>
 
                         <Route exact path={PATHS.SETTINGS.root} component={SettingsPage}/>
 
---
+++ src/features/pages.wrapper.tsx
@@ -0,0 +1,33 @@
+/* Wrapper */
+import {withConnectionStatus} from "../commons/hocs/withConnectionStatus/withConnectionStatus.hoc";
+
+/*Scan*/
+import ScanMenuPage from "./computer/ScanMenu/ScanMenu.page";
+
+/*Computers*/
+import AddComputerPage from "./computer/AddComputer/AddComputer.page";
+import EditComputerPage from "./computer/EditComputer/EditComputer.page";
+import FindComputerPage from "./computer/FindComputer/FindComputer.page";
+
+/*Courses*/
+import AddCoursePage from "./course/AddCourse/AddCourse.page";
+
+/*Loans*/
+import CreateCollectiveLoanPage from "./loan/CreateCollectiveLoan/CreateCollectiveLoan.page";
+import CreateIndividualLoanPage from "./loan/CreateIndividualLoan/CreateIndividualLoan.page";
+import EditIndividualLoanPage from "./loan/EditIndividualLoan/EditIndividualLoan.page";
+import EndLoanPage from "./loan/EndLoan/EndLoan.page";
+
+const WrappedPages = {
+    scanMenu: withConnectionStatus(ScanMenuPage),
+    addComputer: withConnectionStatus(AddComputerPage),
+    editComputer: withConnectionStatus(EditComputerPage),
+    findComputer: withConnectionStatus(FindComputerPage),
+    addCourse: withConnectionStatus(AddCoursePage),
+    createCollectiveLoan: withConnectionStatus(CreateCollectiveLoanPage),
+    createIndividualLoan: withConnectionStatus(CreateIndividualLoanPage),
+    editIndividualLoan: withConnectionStatus(EditIndividualLoanPage),
+    endLoan: withConnectionStatus(EndLoanPage),
+}
+
+export default WrappedPages;
