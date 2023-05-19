1250efc9cc3090120d536a48855eac3f7208e810
0h My Goat <daftouney@gmail.com>
2023-05-19 21:45:16 +0200

    feat: WIP - autoredirect

--- src/commons/hocs/withConnectionStatus/withConnectionStatus.hoc.tsx
+++ src/commons/hocs/withConnectionStatus/withConnectionStatus.hoc.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import useCheckApiConnection from "../../hooks/connection/useCheckApiConnection";
 import {Redirect} from "react-router-dom";
 import PATHS from "../../constants/PATHS";
+import useStorage from "../../hooks/storage/useStorage";
 
 export const withConnectionStatus = (Component: React.FC) => (props: any) => {
 
-    const {isLoanApiConnected} = useCheckApiConnection();
+    const {isConnected} = useCheckApiConnection();
 
-    if (!isLoanApiConnected) return <Redirect to={PATHS.SETTINGS.root}/>;
+    if (!isConnected) return <Redirect to={PATHS.SETTINGS.root}/>;
+
     return <Component {...props}/>;
 
 }
--- src/commons/hooks/connection/useCheckApiConnection.tsx
+++ src/commons/hooks/connection/useCheckApiConnection.tsx
@@ -1,45 +1,61 @@
 import React from 'react';
+import useStorage from "../storage/useStorage";
 
 /**
  * Vérifie la connection à l'API.
  * Ne vérifie que l'API des prêts car il n'est pas nécessaire en l'état de vérifier les autres.
  */
 function useCheckApiConnection() {
-    const [isLoanApiConnected, setLoanApiConnected] = React.useState<boolean>(!!localStorage.getItem('ip'));
+    const {storage} = useStorage();
+    const [isConnected, setConnected] = React.useState<boolean>(false);
     const [isLoading, setLoading] = React.useState<boolean>(false);
-    const [ip, setIp] = React.useState<string>(localStorage.getItem('ip') || '');
+    const [ip, setIp] = React.useState<string>('');
 
 
-    function testConnection() {
+    async function testConnection() {
+
         setLoading(true);
-        setLoanApiConnected(false);
 
-        fetch(`http://${ip}/api/loans/status/health`).then((response) => {
-            if (response.status === 200) {
-                localStorage.setItem('ip', ip);
-                setLoanApiConnected(true);
+        fetch(`http://${ip}/api/loans/status/health`)
+            .then((response) => {
+                if (response.status === 200) {
+                    (async () => {
+                        await storage.set('ip', ip)
+                        await storage.set('isConnected', true)
+                    })()
+                    setConnected(true);
+                }
+            })
+            .catch(() => {
+                setConnected(false);
+            })
+            .finally(() => {
                 setLoading(false)
-            }
-        }).catch(() => {
-            setLoading(false)
-        });
+            });
     }
 
     React.useEffect(() => {
-        if (!localStorage.getItem('ip')) {
-            ip && testConnection()
-        } else {
-            setIp(localStorage.getItem('ip') || '');
-            setLoanApiConnected(true);
-        }
+        (async () => {
+            !isConnected && ip && await testConnection();
+        })();
     }, [ip])
 
+    React.useEffect(() => {
+        (async () => {
+            const ip = await storage.get('ip');
+            const isConnected = await storage.get('isConnected');
+            setIp(ip);
+            setConnected(isConnected);
+        })();
+    }, [])
+
+
     return {
-        isLoanApiConnected,
-        setLoanApiConnected,
+        isConnected,
+        setConnected,
         isLoading,
-        ip,
-        setIp
+        setIp,
+        ip
     }
 
 }
